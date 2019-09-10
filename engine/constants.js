f064d388cad9e2742a0611a6fb6197a59250ac28
Ryan Chen <xryanchen@gmail.com>
2019-09-10 11:28:00 -0700

    feat: rewrite game

---
+++ engine/constants.js
@@ -0,0 +1,48 @@
+/***** util constants *****/
+const EMPTY_FN = () => {}
+
+/***** object props *****/
+// game object
+const [
+  GAME_CONTAINER,
+  GAME_TILE_WIDTH,
+  GAME_TILE_HEIGHT,
+  GAME_CAMERA_WIDTH,
+  GAME_CAMERA_HEIGHT,
+  GAME_BACKGROUND_COLOR,
+  GAME_FLAG_DISABLE_MOVE,
+  GAME_INCIDENT_PLAYS,
+  GAME_INCIDENTS,
+  GAME_LAYERS,
+  GAME_KEY_INTERACTIONS,
+  GAME_LOOP,
+  GAME_CAMERA,
+  GAME_SPRITES,
+  GAME_SOUNDS,
+  GAME_DIALOG
+] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+// interaction object
+const [INTERACTION_KEY_CODES, INTERACTION_PRESS_START_TIME, INTERACTION_PRESS_DURATION, INTERACTION_IS_DOWN] = [
+  0,
+  1,
+  2,
+  3,
+]
+
+// sprite object
+// sound object
+// renderer object
+// incidentRecord
+const [
+  INCIDENT_RECORD_TIMESTAMPS,
+  INCIDENT_RECORD_INCIDENT,
+  INCIDENT_RECORD_PLAY_INCIDENT
+] = [0, 1, 2]
+// incident
+// tile sprite
+// object sprite
+// dialog
+const [
+  DIALOG_START
+] = [0]
--- engine/game.js
+++ engine/game.js
@@ -1,23 +1,4 @@
-// For game object
-const [
-  GAME_CONTAINER,
-  GAME_TILE_WIDTH,
-  GAME_TILE_HEIGHT,
-  GAME_CAMERA_WIDTH,
-  GAME_CAMERA_HEIGHT,
-  GAME_BACKGROUND_COLOR,
-  GAME_FLAG_DISABLE_MOVE,
-  GAME_INCIDENT_PLAYS,
-  GAME_INCIDENT_MAP,
-  GAME_LAYERS,
-  GAME_KEYS,
-  GAME_LOOP,
-  GAME_CAMERA,
-  GAME_SPRITES,
-  GAME_SOUNDS,
-] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-
-function game_create(props = []) {
+function game_factory(props = []) {
   return util_assignArr([], [
     document.createElement('div'),
     20,
@@ -27,7 +8,7 @@ function game_create(props = []) {
     palette_gunmetal[4],
     false,
     [],
-    {},
+    [],
     [],
     [],
     [],
@@ -36,56 +17,100 @@ function game_create(props = []) {
   ], props) 
 }
 
-// For key object
-const [KEY_KEY_CODES, KEY_PRESS_START_TIME, KEY_PRESS_DURATION, KEY_IS_DOWN] = [
-  0,
-  1,
-  2,
-  3,
-]
-
-function game_createKeyInteraction(keyCodes) {
-  const keyObj = [[], null, 0, false]
+function _game_createKeyInteraction(keyCodes = []) {
+  const interaction = util_assignArr([[], null, 0, false], keyCodes)
 
   document.addEventListener('keydown', evt => {
-    if (keyObj[KEY_KEY_CODES].indexOf(evt.keyCode) === -1) {
+    if (interaction[INTERACTION_KEY_CODES].indexOf(evt.keyCode) === -1) {
       return
     }
 
-    keyObj[KEY_IS_DOWN] = true
+    interaction[INTERACTION_IS_DOWN] = true
     const now = Date.now()
-    if (!keyObj[KEY_PRESS_START_TIME]) {
-      keyObj[KEY_PRESS_START_TIME] = now
+    if (!interaction[INTERACTION_PRESS_START_TIME]) {
+      interaction[INTERACTION_PRESS_START_TIME] = now
     } else {
-      keyObj[KEY_PRESS_DURATION] = now - keyObj[KEY_PRESS_START_TIME]
+      interaction[INTERACTION_PRESS_DURATION] = now - interaction[INTERACTION_PRESS_START_TIME]
     }
 
     evt.preventDefault()
   })
 
   document.addEventListener('keyup', evt => {
-    if (keyObj[KEY_KEY_CODES].indexOf(evt.keyCode) === -1) {
+    if (interaction[INTERACTION_KEY_CODES].indexOf(evt.keyCode) === -1) {
       return
     }
 
-    keyObj[KEY_IS_DOWN] = false
-    keyObj[KEY_PRESS_START_TIME] = null
-    keyObj[KEY_PRESS_DURATION] = 0
+    interaction[INTERACTION_IS_DOWN] = false
+    interaction[INTERACTION_PRESS_START_TIME] = null
+    interaction[INTERACTION_PRESS_DURATION] = 0
 
     evt.preventDefault()
   })
 
-  return keyObj
+  return interaction
 }
 
-function game_loadSprites(_game, _sprites) {
-  _game[GAME_SPRITES] = _sprites
+function game_loadSprites(game, sprites = []) {
+  game[GAME_SPRITES] = sprites
 }
 
-function game_loadSounds(_game, _sounds) {
-  _game[GAME_SOUNDS] = _sounds
+function game_loadSounds(game, sounds = []) {
+  game[GAME_SOUNDS] = sounds
 }
 
-function game_addInteractionKey() {
+function game_addInteractionKey(game, keyId = -1, keyCodes = []) {
+  game[GAME_KEY_INTERACTIONS][keyId] = _game_createKeyInteraction(keyCodes)
+}
 
+function game_addLayer(game, layerId = -1, renderer = []) {
+  game[GAME_LAYERS][layerId] = renderer
+}
+
+function game_addIncident(game, incidentId = -1, incidentFactory = EMPTY_FN, incidentProps = [], isForced = false) {
+  const cachedIncidentRecord = game[GAME_INCIDENTS][incidentId]
+  const incidentRecord = !isForced && cachedIncidentRecord ? cachedIncidentRecord : [[], incidentFactory(game, incidentProps)]
+  
+  incidentRecord[INCIDENT_RECORD_TIMESTAMPS].push(Date.now())
+  game[GAME_INCIDENTS][incidentId] = incidentRecord
+
+  // start or restart the incident
+  const incident = incidentRecord[INCIDENT_RECORD_INCIDENT]
+  // make sure incident has updated playerStatus
+  incident[INCIDENT_PLAYER_STATUS] = incidentProps[INCIDENT_PLAYER_STATUS]
+  incident_restart()
+
+  // add play method into incident plays queue and dedupe
+  incidentRecord[INCIDENT_RECORD_PLAY_INCIDENT] = incidentRecord[INCIDENT_RECORD_PLAY_INCIDENT] || incident[INCIDENT_PLAY].bind(null, incident)
+  const incidentPlayIndex = game[GAME_INCIDENT_PLAYS].indexOf(incidentRecord[INCIDENT_RECORD_PLAY_INCIDENT])
+  if (incidentPlayIndex >= 0) {
+    game[GAME_INCIDENT_PLAYS].splice(incidentPlayIndex, 1)
+  }
+  game[GAME_INCIDENT_PLAYS].push(incidentRecord[INCIDENT_RECORD_PLAY_INCIDENT])
+}
+
+function game_createTileSprite(game, spriteId = -1, spriteFactory = EMPTY_FN, tileSpriteProps = []) {
+  game[GAME_SPRITES][spriteId] = spriteFactory(tileSpriteProps)
+}
+
+function game_createObjectSprite(game, spriteId = -1, spriteFactory = EMPTY_FN, objectSpriteProps = []) {
+  game[GAME_SPRITES][spriteId] = spriteFactory(objectSpriteProps)
+}
+
+function game_playConversation(game, dialogFactory = EMPTY_FN, dialogProps = [], callback = EMPTY_FN) { 
+  game[GAME_DIALOG] = dialogFactory(dialogProps)
+  game[GAME_DIALOG][DIALOG_START]()
+  game[GAME_DIALOG][DIALOG_END_CALLBACKS].push(callback, (() => game_setDialog(game, null)))
+}
+
+function game_setDialog(game, dialog = null) {
+  game[GAME_DIALOG] = dialog
+}
+
+function game_pause(game) {
+  game[GAME_FLAG_DISABLE_MOVE] = true
+}
+
+function game_resume(game) {
+  game[GAME_FLAG_DISABLE_MOVE] = false
 }
